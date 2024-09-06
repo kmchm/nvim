bbb0380c0b6e93b8c5564b26bc35b1e197fdc1cc
Kamil Chmielewski <kamil.hopman@gmail.com>
2024-09-06 17:29:03 +0200

    add basic nvim configuration

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+require("vim-options")
+require("lazy").setup("plugins")
+
---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2 smarttab")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set cursorline")
+vim.cmd("set ttyfast")
+vim.cmd("syntax on")
+vim.cmd("set number")
+vim.wo.relativenumber = true
+
+vim.g.mapleader = " "
