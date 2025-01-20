b88d0165dfb2b1643045d5f4aae32bb8f6b817f0
Kamil <kamil.hopman@gmail.com>
2025-01-20 17:22:26 +0100

    init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function() 
+         
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = {
+                "c", 
+                "lua", 
+                "vim", 
+                "vimdoc", 
+                "query", 
+                "elixir", 
+                "heex", 
+                "javascript", 
+                "java",
+                "html"
+            },
+            sync_install = false,
+            highlight = {
+                enable = true
+            },
+            indent = {
+                enable = true
+            }
+        })
+    end
+
+
+} 
