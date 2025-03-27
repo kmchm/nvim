2872e2ca190da944636266c77b0790b35c7b1597
Kamil <mail@kamilch.com>
2025-03-27 13:26:57 +0100

    init

---
+++ lua/vim-options.lua
@@ -0,0 +1,13 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4 smarttab")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set cursorline")
+vim.cmd("set ttyfast")
+vim.cmd("syntax on")
+vim.cmd("set number")
+vim.opt.ignorecase = true
+vim.wo.relativenumber = true
+
+vim.g.mapleader = " "
